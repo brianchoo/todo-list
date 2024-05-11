4c620be2c129f48d1afa9f06eb44bcdece82e49b
brian2012 <brianchoo2012@gmail.com>
2024-05-11 21:46:03 +0800

    feat: added AddTodo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import TodoListHeader from "./components/TodolistHeader";
+import TodoListHeader from "./components/TodoListHeader";
+import AddTodo from "./components/AddTodo";
 
 const App = () => {
   return (
@@ -6,6 +7,7 @@ const App = () => {
       <main className="h-screen w-full flex justify-center items-center ">
         <div className="w-10/12 md:w-1/4 h-auto p-4 rounded-md bg-lime-100">
           <TodoListHeader />
+          <AddTodo />
         </div>
       </main>
     </>
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const AddTodo = () => {
+  const [inputTodo, setInputTodo] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputTodo(e.target.value);
+  };
+
+  return (
+    <div className="mt-4">
+      <input
+        onChange={handleInputChange}
+        type="text"
+        name="title"
+        className="block w-full rounded-md border py-1.5 pl-4 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring focus:ring-lime-200 focus:border focus:border-lime-500"
+        placeholder="What needs to be done?"
+      />
+      <div className="flex justify-end mt-5">
+        <button type="button" className="hover:underline">
+          Cancel
+        </button>
+        <button
+          className="bg-green-400 hover:text-white text-green-700 font-semibold py-1.5 px-4 rounded-md disabled:bg-gray-200 disabled:text-gray-300 disabled:border-slate-200 disabled:shadow-none ml-4"
+          disabled={!inputTodo.trim()}
+        >
+          Create
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTodo;
