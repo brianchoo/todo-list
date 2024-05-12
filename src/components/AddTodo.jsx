684b4df9dc21f08bbf1aa9014000af6019f001bd
brian2012 <brianchoo2012@gmail.com>
2024-05-12 12:19:07 +0800

    feat: clear todo input field after submit

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -7,12 +7,18 @@ const AddTodo = ({ onToggleCloseTodo, onAddTodo }) => {
     setInputTodo(e.target.value);
   };
 
+  const handleAddTodo = () => {
+    onAddTodo(inputTodo);
+    setInputTodo("");
+  };
+
   return (
     <div className="mt-4">
       <input
         onChange={handleInputChange}
         type="text"
         name="title"
+        value={inputTodo}
         className="block w-full rounded-md border py-1.5 pl-4 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring focus:ring-lime-200 focus:border focus:border-lime-500"
         placeholder="What needs to be done?"
       />
@@ -23,7 +29,7 @@ const AddTodo = ({ onToggleCloseTodo, onAddTodo }) => {
         <button
           className="bg-green-400 hover:text-white text-green-700 font-semibold py-1.5 px-4 rounded-md disabled:bg-gray-200 disabled:text-gray-300 disabled:border-slate-200 disabled:shadow-none ml-4"
           disabled={!inputTodo.trim()}
-          onClick={() => onAddTodo(inputTodo)}
+          onClick={handleAddTodo}
         >
           Create
         </button>
